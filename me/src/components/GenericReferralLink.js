a08913efc8ddb93baa65b0d031ddef7a2658f14f
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-11-23 14:59:53 -0800

    WIP

--- me/src/components/GenericReferralLink.js
+++ me/src/components/GenericReferralLink.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class GenericReferralLink extends Component {
+	handleClick(e) {
+		e.preventDefault();
+		console.log("send back to aaron")
+	}
 	render() {
 		return (
-			   <a target="_blank" href="http://jump2click.com/visit/?bta=37140&nci=6639">{this.props.linkText}</a>
+			   <a onClick={this.handleClick} target="_blank" href="hhttp://jump2click.com/visit/?bta=37140&nci=6639">{this.props.linkText}</a>
 		)
 	}
 }
